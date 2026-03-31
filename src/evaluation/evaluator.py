49e6c4f748d5aa15eba80d46b93852670602c159
fenglin <1349410043@qq.com>
2026-03-31 18:39:56 +0800

    eval debug

--- src/evaluation/evaluator.py
+++ src/evaluation/evaluator.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 from torch.utils.data import Dataset
 from src.evaluation.predictor import predict_label
 from src.evaluation.metrics import compute_classification_metrics
-from src.datasets.schemas import LABEL2ID, ID2LABEL
+from src.datasets.schemas import LABEL2ID, ID2LABEL, Sample
 
 @torch.no_grad()
 def evaluate(model, tokenizer, dataset: Dataset, device: torch.device, max_new_tokens: int = 128):
@@ -13,7 +13,7 @@ def evaluate(model, tokenizer, dataset: Dataset, device: torch.device, max_new_t
     gold_ids = []
 
     for i in tqdm(range(len(dataset)), desc="Eval"):
-        sample = dataset[i]
+        sample: Sample = dataset[i]
         pred_label, _ = predict_label(
             model=model,
             tokenizer=tokenizer,
@@ -22,7 +22,7 @@ def evaluate(model, tokenizer, dataset: Dataset, device: torch.device, max_new_t
             max_new_tokens=max_new_tokens,
         )
 
-        gold_id = int(sample["label"])
+        gold_id = int(sample.label)
         gold_ids.append(gold_id)
 
         if pred_label is None:
