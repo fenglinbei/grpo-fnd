b91c260e798d6d76d6b09bf1792f2c957628926f
fenglin <1349410043@qq.com>
2026-04-02 12:10:53 +0800

    change config

--- src/training/train_grpo.py
+++ src/training/train_grpo.py
@@ -88,6 +88,7 @@ def train_grpo_epoch(
 
         # 处理奖励细节，方便后续记录
         processed_rewards_details = {}
+        logger.debug("Processing reward details for logging | rewards_details={}", rewards_details)
         for key in rewards_details[0][0].keys():
             processed_rewards_details[key] = np.mean([
                 rewards_details[b][g][key] for g in range(group_size) for b in range(batch_size)
